2aaa9ca7788bd88d3249213e85c984b7d2f8f1f8
leticiadesouza <leticia.souza212@gmail.com>
2018-06-22 10:14:12 -0300

    Create possible structure to test

---
+++ test/test_diskspace.py
@@ -0,0 +1,13 @@
+import unittest
+import subprocess
+import os
+
+from diskspace import diskspace
+
+class BytesToReadableTest(unittest.TestCase):
+
+    # Tests relative to the bytes_to_readable method.
+
+class SubprocessCheckOutputTest(unittest.TestCase):
+
+    # Tests relative to the subprocess_check_output method.
