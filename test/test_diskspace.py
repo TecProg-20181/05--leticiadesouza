10bc8bc0aee34bda15c1a5afdbc0da6920e56880
leticiadesouza <leticia.souza212@gmail.com>
2018-06-22 11:50:20 -0300

    Add tests to return values to test

--- test/test_diskspace.py
+++ test/test_diskspace.py
@@ -6,8 +6,23 @@ from diskspace import diskspace
 
 class BytesToReadableTest(unittest.TestCase):
 
-    # Tests relative to the bytes_to_readable method.
+    def test_bytes_values(self):
+        blocks = 0
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        self.assertEqual('0.00B', bytes_size)
+
+
+    def test_bytes_values(self):
+        blocks = 1
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        self.assertEqual('512.00B', bytes_size)
+
+
+    def test_return_type(self):
+        blocks = 0
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        self.assertIsInstance(bytes_size, str)
+
 
 class SubprocessCheckOutputTest(unittest.TestCase):
-
-    # Tests relative to the subprocess_check_output method.
+  # Tests relative to the subprocess_check_output method.
