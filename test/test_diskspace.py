63bb22e3b6cd1a2b7c63eff863a6fe0b7b46e78b
leticiadesouza <leticia.souza212@gmail.com>
2018-06-22 12:01:54 -0300

    Add tests to each block with bytes

--- test/test_diskspace.py
+++ test/test_diskspace.py
@@ -24,5 +24,28 @@ class BytesToReadableTest(unittest.TestCase):
         self.assertIsInstance(bytes_size, str)
 
 
-class SubprocessCheckOutputTest(unittest.TestCase):
-  # Tests relative to the subprocess_check_output method.
+    def test_tbytes(self):
+        blocks = 10000000000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Tb', label)
+
+
+    def test_gbytes(self):
+        blocks = 10000000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Gb', label)
+
+
+    def test_mbytes(self):
+        blocks = 100000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Mb', label)
+
+    def test_kbytes(self):
+        blocks = 1000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Kb', label)
